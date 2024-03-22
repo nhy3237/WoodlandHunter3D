bff449f9bfec66292561217dfc4f4784379589f1
nhy3237 <njs3237@naver.com>
2024-03-22 17:13:44 +0900

    Feat : Player FSM, Animator
    24/03/22

---
+++ Assets/Scripts/Player/Aira/FSM/StateMachine.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class StateMachine<T> where T : class
+{
+    private T ownerEntity;
+    private State<T> currentState;
+
+    public void Setup(T owner, State<T> entryState)
+    {
+        ownerEntity = owner;
+        currentState = null;
+
+        ChangeState(entryState);
+    }
+
+    public void Execute()
+    {
+        if (currentState != null)
+        {
+            currentState.Execute(ownerEntity);
+        }
+    }
+
+    public void ChangeState(State<T> newState)
+    {
+        if (newState == null) return;
+
+        if (currentState != null)
+        {
+            currentState.Exit(ownerEntity);
+        }
+
+        currentState = newState;
+        currentState.Enter(ownerEntity);
+    }
+}
