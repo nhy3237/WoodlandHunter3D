0d1bd50c501d1d3f29880ad243e6d4a99b39ecb0
nhy3237 <njs3237@naver.com>
2024-03-25 17:23:14 +0900

    Update : Prefabs 폴더 추가 및 문서 정리, 플레이어 Walk
    24/03/25 희영

--- Assets/Scripts/Player/Player_Aira/State/AiraPlayerWalkState.cs
+++ Assets/Scripts/Player/Player_Aira/State/AiraPlayerWalkState.cs
@@ -20,8 +20,13 @@ public class AiraPlayerWalkState : State<AiraPlayerController>
 
     public override void Execute(AiraPlayerController entity)
     {
-        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
-        entity.transform.Translate(movement);
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput);
+        entity.transform.parent.Translate(movement * moveSpeed * Time.deltaTime);
+
+        animator.SetFloat("Horizontal", horizontalInput);
     }
 
     public override void Exit(AiraPlayerController entity)
